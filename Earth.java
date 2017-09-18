64741cdcfcf35b60d65dbe9057063ce197ac5cc1
chiranjeeb2410 <chiranjeebm2410@gmail.com>
2017-09-18 10:18:13 -0400

    intro to OOP class and object

---
+++ Earth.java
@@ -0,0 +1,16 @@
+public class Earth	{
+	
+	public static void main(String args[])	{
+		Human tom;
+		tom = new Human();
+		tom.age = 21;
+		tom.name = "Tom";
+		tom.heightInInches = 167;
+		tom.speak();
+		tom.eat();
+		tom.walk();
+		tom.height();
+	}
+
+}
+		
---
+++ Human.java
@@ -0,0 +1,29 @@
+public class Human	{
+
+	String name;
+	int age;
+	int heightInInches;
+	String eyeColor;
+
+	public Human()	{
+	
+	}
+
+	void speak()	{
+		System.out.println("Hello, my name is " + name);
+	}
+
+	void eat()	{
+		System.out.println("Eating...");
+	}
+
+	void walk()	{
+		System.out.println("Walking....");
+	}
+
+	void height()	{
+		System.out.println("My height is " + heightInInches + " cms");
+	}
+
+}
+	

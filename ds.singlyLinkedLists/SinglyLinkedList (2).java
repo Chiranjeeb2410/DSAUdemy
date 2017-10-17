120e7d1ae8e9b084245905cc1d8d264d6a6fe3e5
Vennela <chava.vinni@gmail.com>
2017-10-17 18:12:58 +0530

    Change ds.circularLL to ds.circularLinkedLists

---
+++ ds.singlyLinkedLists/SinglyLinkedList (2).java
@@ -0,0 +1,42 @@
+public class SinglyLinkedList   {
+
+    public Node first;
+
+    public boolean isEmpty()    {
+        return (first == null);
+    }
+
+    public void insertFirst(int data)   {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = first;
+        first = newNode;
+    }
+
+    public Node deleteFirst()   {
+        Node temp = first;
+        first = first.next;
+        return temp;
+    }
+
+    public void displayList()   {
+        System.out.println("The list is: ");
+        Node currentNode = first;
+        while(currentNode != null)  {
+            currentNode.displayNode();
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public void insertLast(int data)    {
+        Node currentNode = first;
+        while(currentNode.next != null)  {
+            currentNode = currentNode.next;
+        }
+
+        Node newNode = new Node();
+        newNode.data = data;
+        currentNode.next = newNode;
+    }
+}

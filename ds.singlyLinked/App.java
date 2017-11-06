a5a0f8c3eedadb751977aa04066401b10c4ef895
chiranjeeb2410 <chiranjeebm2410@gmail.com>
2017-11-06 09:56:14 -0500

    added changes

--- ds.singlyLinked/App.java
+++ ds.singlyLinked/App.java
@@ -3,7 +3,8 @@ public class App    {
     public static void main(String[] args)  {
 
         SinglyLinkedList theList = new SinglyLinkedList();
-        theList.deleteFirst();
+        theList.insertFirst(100);
+        theList.insertFirst(99);
 
         theList.displayList();
         System.out.println();
--- ds.singlyLinked/SinglyLinkedList.java
+++ ds.singlyLinked/SinglyLinkedList.java
@@ -24,24 +24,24 @@ public class SinglyLinkedList   {
         }
     }
 
+    public void insertLast(int data)    {
+        Node currentNode = first;
+        while(currentNode.next != null)  {
+            currentNode = currentNode.next;
+        }
+        Node newNode = new Node();
+        newNode.data = data;
+        currentNode.next = newNode;
+    }
+
     public void displayList()   {
         System.out.println("The list is: ");
         Node currentNode = first;
-        while(currentNode != null)  {
+        while(currentNode.next != null)  {
             currentNode.displayNode();
             currentNode = currentNode.next;
         }
         System.out.println();
     }
 
-    public void insertLast(int data)    {
-        Node currentNode = first;
-        while(currentNode.next != null)  {
-            currentNode = currentNode.next;
-        }
-
-        Node newNode = new Node();
-        newNode.data = data;
-        currentNode.next = newNode;
-    }
 }
